87beb6c937da2fe72fc076e965df3187e0cfa197
hedhed hedhed <hedy_12@hotmail.fr>
2023-11-15 18:55:15 +0100

    tags ok

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidBody= GetComponent<Rigidbody>();
+        renderer.enabled = false;
+        rigidBody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+        if(Time.time > timeToWait){
+            renderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
     }
 }
